9b5e1d154159a7481ced32ef0eb9a7c3d6bace14
JunQi123-del <76680865+JunQi123-del@users.noreply.github.com>
2021-05-19 18:16:17 +0800

    Introduced Variables

--- site.php
+++ site.php
@@ -5,8 +5,20 @@
 	</head>
 	<body>
 		
-		<?php
-		echo("Hello World"); //Print 
+		<?php // <?php ..... anything run inside will be php script
+
+		$charName = "John"; //$ is used to create variables
+		$charAge = 21;
+		$isMale = true; // boolean variable 
+
+		echo "<h1> Jun Qi's Site</h1>";
+		echo "<hr>";
+		echo("Hello World <br>"); //echo = Print 
+
+		echo "i am here <br>"; //<br> = break/newline
+		echo "nice<br>";
+		echo "my name is $charName and my age is $charAge <br>";
+
 		
 		?>
 		
